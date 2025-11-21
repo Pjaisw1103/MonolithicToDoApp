4132678b223aff899ec884204a843f7d671ca722
Pjaisw1103 <priyajaisw9554@gmail.com>
2025-11-21 23:45:25 +0530

    Added Once Again

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -10,6 +10,7 @@ terraform {
     storage_account_name = "tfstate1230"
     container_name       = "tfcontainer"
     key                  = "demo.terraform.tfstate"
+    use_azuread_auth     = true
   }
 }
 
